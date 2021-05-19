85df9476fa2644cccf1277e5b1c3d63388dd9c74
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2021-05-19 12:21:47 +0530

    added other fields to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :f_name, :l_name, :designation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:f_name,:l_name,:designation])
+  end
 end
