4400102fbcb5ccc38f535c4e4819566a9d90073e
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2021-05-27 18:46:12 +0530

    change in gemfile in sqlite

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Meets
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.1 
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
